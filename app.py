f7ab67c50152c426504082c99213b71e0dc35d30
Avi Romanoff <avi@romanoff.me>
2017-04-28 14:09:37 -0400

    MongoDB

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
+import os
 from flask import Flask
-from datetime import datetime
+from flask.ext.pymongo import PyMongo
+
 app = Flask(__name__)
+app.config['MONGO_URI'] =  os.environ['BUBBLETEAM_MONGO_URI']
+mongo = PyMongo(app)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
+    foos = mongo.db.foo.find()
     return """
-    <h1>Hello bubble team</h1>
-    <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    <h1>Hello bubble team 🛁🍵</h1>
+    {foo}
+    """.format(foo=foos[0])
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
